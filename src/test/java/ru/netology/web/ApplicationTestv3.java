abf046a4b9aac88e55242602beff545a16226b36
Aleksandr Kharitonovich <harison-din@yandex.ru>
2023-01-31 13:27:27 +0300

    Тесты v3

--- src/test/java/ru/netology/web/ApplicationTestv3.java
+++ src/test/java/ru/netology/web/ApplicationTestv3.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.cssClass;
@@ -7,11 +8,15 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class ApplicationTest {
+public class ApplicationTestv3 {
+
+    @BeforeEach
+    public void initEach() {
+        open("http://localhost:9999");
+    }
 
     @Test
     public void shouldCriticalPathTest() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
@@ -22,7 +27,6 @@ public class ApplicationTest {
 
     @Test
     public void shouldValidNameTestOne() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Фёдорова Алёна");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
@@ -33,7 +37,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldValidNameTestTwo() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Салтыков-Щедрин Михаил");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
@@ -44,7 +48,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldValidNameTestThree() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Мухаммад ибн Абд аль-Ваххаб");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
@@ -55,7 +59,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidNameTestOne() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Alex Kharitonovich");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
@@ -66,7 +70,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidNameTestTwo() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Aleksandr");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
@@ -77,7 +81,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidNameTestThree() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("+79161111111");
         $("[data-test-id=phone] input").setValue("+79161111111");
         $("[data-test-id=agreement]").click();
@@ -88,7 +92,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidPhoneTestOne() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=phone] input").setValue("89166760000");
         $("[data-test-id=agreement]").click();
@@ -99,7 +103,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidPhoneTestTwo() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=phone] input").setValue("+7916676000");
         $("[data-test-id=agreement]").click();
@@ -110,7 +114,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidPhoneTestThree() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=phone] input").setValue("+791667600000");
         $("[data-test-id=agreement]").click();
@@ -121,7 +125,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldInvalidPhoneTestFour() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=phone] input").setValue("Iphone");
         $("[data-test-id=agreement]").click();
@@ -132,7 +136,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldEmptyInputTestOne() {
-        open("http://localhost:9999");
+
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
@@ -142,7 +146,7 @@ public class ApplicationTest {
 
     @Test
     public void shouldEmptyInputTestTwo() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
@@ -152,10 +156,12 @@ public class ApplicationTest {
 
     @Test
     public void shouldEmptyCheckBoxTest() {
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Харитонович Александр");
         $("[data-test-id=phone] input").setValue("+79166760000");
         $("button.button").click();
         $("[data-test-id=agreement]").should(cssClass("input_invalid"));
+
+
     }
 }
